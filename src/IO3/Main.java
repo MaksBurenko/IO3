2006d3cbe854faa683c819657fbee081b0990f4b
Maksim Burenko <Cherniymaks@yandex.ru>
2022-06-18 16:43:46 +0300

    123

--- src/IO3/Main.java
+++ src/IO3/Main.java
@@ -2,17 +2,20 @@ package IO3;
 
 import java.io.*;
 import java.nio.charset.Charset;
-import java.util.Arrays;
 
 public class Main {
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, charset));
+
         String sum = "";
-        String s;
-        while ((s = reader.readLine()) != null) {
-            sum = sum + s;
-        }
+        int s = 0;
+        char p =0;
+
+       while ((s = reader.read()) != -1) {
+           p = (char)s;
+           sum = sum + p;
+       }
         reader.close();
         return sum;
     }
